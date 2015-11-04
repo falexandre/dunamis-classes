5ab24e4f5841e60ec2e36e21329fe0d41bd7344f
Fábio falexandre <fabiolalexandre@gmail.com>
2015-11-04 09:44:53 -0200

    SEO FUNCTION AJUST

--- DunamisClasses/funcoes.php
+++ DunamisClasses/funcoes.php
@@ -26,11 +26,13 @@ class funcoes
 			$c     = 2 * atan2(sqrt($a), sqrt(1 - $a));	       
 	    return ($formato == 'M') ? round($r * $c * 1000) : round($r * $c); // resultado em metros.
 	}
+
+	
 	
 	public static function urlSeo($str){
 
-	$str = strtolower(utf8_decode($str)); $i=1;
-	    $str = strtr($str, utf8_decode('àáâãäåæçèéêëìíîïñòóôõöøùúûýýÿ'), 'aaaaaaaceeeeiiiinoooooouuuyyy');
+		$str = strtolower(utf8_decode($str)); $i=1;
+	    $str = strtr($str, utf8_decode('ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÑÒÓÔÕÖØÙÚÛÝÝŸàáâãäåæçèéêëìíîïñòóôõöøùúûýýÿ'), 'aaaaaaaceeeeiiiinoooooouuuyyyaaaaaaaceeeeiiiinoooooouuuyyy');
 	    $str = preg_replace("/([^a-z0-9])/",'-',utf8_encode($str));
 	    while($i>0) $str = str_replace('--','-',$str,$i);
 	    if (substr($str, -1) == '-') $str = substr($str, 0, -1);
